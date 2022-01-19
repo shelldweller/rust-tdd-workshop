097e4679a9a261c208648429e4776131fc7a7eae
Sergiy Kuzmenko <sergiy@browze.com>
2022-01-19 11:53:04 -0500

    Add plateau

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,27 @@ struct Rover {
     direction: Direction
 }
 
+// -- Plateau
+
+struct Plateau {
+    sw: Point,
+    ne: Point,
+}
+
+impl Plateau {
+    fn new(sw: Point, ne: Point) -> Self {
+        Self {
+            sw: sw,
+            ne: ne,
+        }
+    }
+
+    fn is_valid(&self, point: Point) -> bool {
+        point.x <= self.ne.x && point.x >= self.sw.x && point.y <= self.ne.y && point.y >= self.sw.y
+    }
+
+}
+
 impl Rover {
     fn new(name: String, point: Point, direction: Direction) -> Self {
         Self {
@@ -45,7 +66,7 @@ impl Rover {
 
 #[cfg(test)]
 mod tests {
-    use super::{Point, Direction, Rover};
+    use super::{Point, Direction, Rover, Plateau};
 
     #[test]
     fn init_rover() {
@@ -60,8 +81,21 @@ mod tests {
 
     #[test]
     fn init_plateau() {
-        let plateau = Plateau::new(Point(0, 0), Point(100, 100));
-        assert_eq!(plateau.sw, Point(0, 0));
-        assert_eq!(plateau.ne, Point(100, 100));
+        let plateau = Plateau::new(Point::new(0, 0), Point::new(100, 100));
+        assert_eq!(plateau.sw, Point::new(0, 0));
+        assert_eq!(plateau.ne, Point::new(100, 100));
     }
+
+    #[test]
+    fn is_point_on_plateau() {
+        let plateau = Plateau::new(Point::new(1, 1), Point::new(100, 100));
+        assert!(plateau.is_valid(Point::new(1, 1)));
+
+        assert!(!plateau.is_valid(Point::new(0, 1)));
+        assert!(!plateau.is_valid(Point::new(1, 0)));
+        assert!(!plateau.is_valid(Point::new(101, 1)));
+        assert!(!plateau.is_valid(Point::new(1, 101)));
+    }
+
+
 }
